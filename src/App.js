552a43eb9e7fbb30fb2945d69c10b43041c1bc86
Liping Yin <liping.yin@D2WDFHCJYN.local>
2025-04-30 23:22:19 +0200

    add gtm lib

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { auto } from "@cloudinary/url-gen/actions/resize";
+import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
+import { AdvancedImage } from "@cloudinary/react";
 
 function App() {
+  const apiKey = process.env.REACT_APP_AMPLI_API_KEY;
+
+  const cld = new Cloudinary({ cloud: { cloudName: "dwi8mo6ev" } });
+
+  // Use this sample image or upload your own via the Media Explorer
+  const img = cld
+    .image("IMG_4300_gkzshq")
+    .format("auto") // Optimize delivery by resizing and applying auto-format and auto-quality
+    .quality("auto")
+    .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+
   return (
     <div className="App">
-      <div> My Amplitude GTM test App</div>
+      <h1> My Amplitude GTM test App</h1>
       <div>
         <button> Click Me</button>
+      </div>
+      <div>
+        <AdvancedImage cldImg={img} />
       </div>
     </div>
   );
